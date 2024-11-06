cc899b2ffd22211a711899ebbd47a0a10e237084
Erik Zhou <erikxiangzhou@gmail.com>
2024-11-06 23:39:11 +1100

    added the graphql endpoint and make the voyager working fine

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+## Graphql
+http://localhost:8000/graphiql?path=/graphql
+
+```graphql
+query {
+  hello
+  greeting(name: "Test")
+}
+```
--- src/main/kotlin/com/example/demo/GreetingControllerRest.kt
+++ src/main/kotlin/com/example/demo/GreetingControllerRest.kt
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
-@RequestMapping("/")
-class GreetingController {
-    @GetMapping("/{name}")
-    fun retrieveGreeting(
-        @PathVariable("name") name: String,
-    ): String = "Hello $name you are cool oh yeah bala!"
-}
+//@RestController
+//@RequestMapping("/")
+//class GreetingControllerRest {
+//    @GetMapping("/{name}")
+//    fun retrieveGreeting(
+//        @PathVariable("name") name: String,
+//    ): String = "Hello $name you are sexy yeah bala!"
+//}
